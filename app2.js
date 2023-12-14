432ad63bbc32e58c61b85c2ef4b29b4a377950fb
khoihaylam <khoimapdit1234@gmail.com>
2023-12-14 20:37:31 +0700

    first commit

---
+++ app2.js
@@ -0,0 +1,15 @@
+function tinhtong(a, b) {
+  return a + b;
+}
+console.log(tinhtong(3, 5));
+var string = "10*2+30";
+console.log(eval(string));
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9));
+console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9));
+console.log(Math.floor(Math.random() * 10));
+console.log(Math.ceil(-5.5));
+function thongbao() {
+  alert("hello");
+}
+setInterval(thongbao, 2000);
+setTimeout(thongbao, 2000);
